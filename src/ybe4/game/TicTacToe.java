7a32499f538f7024145640ad97474e1038e2c279
Anton Rufino <anton.vongola@gmail.com>
2016-04-07 10:27:46 +0800

    Remove winningPositions attribute from TicTacToe

--- src/ybe4/game/TicTacToe.java
+++ src/ybe4/game/TicTacToe.java
@@ -9,16 +9,6 @@ public class TicTacToe {
     private static TicTacToe instance = new TicTacToe();
 
     char[][] board = new char[3][3];
-    private final static int[][] winningPositions = {
-        {0, 1, 2},
-        {3, 4, 5},
-        {6, 7, 8},
-        {0, 3, 6},
-        {1, 4, 7},
-        {2, 5, 8},
-        {0, 4, 8},
-        {2, 4, 6}
-    };
 
     private TicTacToe() { }
 
