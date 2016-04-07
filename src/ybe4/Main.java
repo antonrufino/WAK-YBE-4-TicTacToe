eb4e888c527d52e728f0bfac8ba1d183b0f3cd49
Anton Rufino <anton.vongola@gmail.com>
2016-04-07 11:06:32 +0800

    Added winner dinner detection and prevented player from placing peices in nonempty positions.

--- src/ybe4/Main.java
+++ src/ybe4/Main.java
@@ -1,16 +1,42 @@
 /**
  * Class name: Main
- * Class description: Main class of program. 
+ * Class description: Main class of program.
  */
 
 package ybe4;
 
 import ybe4.game.TicTacToe;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    private static TicTacToe t;
+
     public static void main(String[] args) {
-        TicTacToe t = TicTacToe.getInstance();
+        char player = 'O';
+        char winner = ' ';
+
+        t = TicTacToe.getInstance();
         t.initBoard();
-        t.printBoard();
+
+        while ((winner = t.getWinner()) == ' ') {
+            actionMenu(player);
+            t.printBoard();
+            player = (player == 'O') ? 'X' : 'O';
+        }
+
+        System.out.println("Winner is " + winner);
+    }
+
+    private static void actionMenu(char player) {
+        int row, col;
+
+        System.out.print("row: ");
+        row = scan.nextInt();
+
+        System.out.print("col: ");
+        col = scan.nextInt();
+
+        t.setPosition(row, col, player);
     }
 }
--- src/ybe4/game/TicTacToe.java
+++ src/ybe4/game/TicTacToe.java
@@ -19,7 +19,7 @@ public class TicTacToe {
     public void initBoard() {
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
-                board[i][j] = ' ';
+                board[i][j] = '-';
     }
 
     public void printBoard() {
@@ -29,5 +29,34 @@ public class TicTacToe {
             }
             System.out.println();
         }
+    }
+
+    public void setPosition(int row, int col, char player) {
+        if (this.board[row][col] == '-')
+            this.board[row][col] = player;
+    }
+
+    public char getWinner() {
+        for (int i = 0; i < 3; ++i) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                if (board[i][0] != '-') return board[i][0];
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+                if (board[0][i] != '-') return board[0][i];
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+                if (board[0][0] != '-') return board[0][0];
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+                if (board[0][2] != '-') return board[0][2];
+        }
+
+        return ' ';
     }
 }
